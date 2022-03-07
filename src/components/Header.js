2ad743eabe8b3fd0ca8abbadda3bdd0a1108aa41
Kaike Yudi <kaikeyudi@outlook.com>
2022-03-07 19:22:36 -0300

    requisito 3: cria lógica para mostrar o nome de usuário no componente Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,38 @@
 import React, { Component } from 'react';
+import { getUser } from '../services/userAPI';
+import Loading from './Loading';
 
 class Header extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      isLoading: false,
+    };
+    this.handleGetUser = this.handleGetUser.bind(this);
+  }
+
+  componentDidMount() {
+    this.handleGetUser();
+  }
+
+  async handleGetUser() {
+    this.setState({
+      isLoading: true,
+    });
+    const { name } = await getUser();
+    this.setState({
+      isLoading: false,
+      name,
+    });
+  }
+
   render() {
+    const { isLoading, name } = this.state;
     return (
-      <header data-testid="header-component">Header</header>
+      <header data-testid="header-component">
+        {isLoading ? <Loading /> : <div data-testid="header-user-name">{name}</div>}
+      </header>
     );
   }
 }
