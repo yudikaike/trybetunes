5740647f212dfaa74ce48045949448d401a52b5b
Kaike Yudi <kaikeyudi@outlook.com>
2022-03-07 18:39:11 -0300

    requisito 2: adiciona eventos aos inputs e cria lógica que desabilita o botão

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,8 +2,30 @@ import React, { Component } from 'react';
 
 class Login extends Component {
   render() {
+    const { isButtonDisabled, handleChange, handleSubmit } = this.props;
     return (
-      <div data-testid="page-login">Login</div>
+      <div data-testid="page-login">
+        <form>
+          <label htmlFor="login-name-input">
+            Login:
+            <input
+              onChange={ handleChange }
+              name="name"
+              type="text"
+              id="login-name-input"
+              data-testid="login-name-input"
+            />
+          </label>
+          <button
+            onClick={ handleSubmit }
+            disabled={ isButtonDisabled }
+            type="button"
+            data-testid="login-submit-button"
+          >
+            Enviar
+          </button>
+        </form>
+      </div>
     );
   }
 }
