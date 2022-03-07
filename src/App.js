c92f68c7d0ecd701b02c74f65a8f94616b3aaaa5
Kaike Yudi <kaikeyudi@outlook.com>
2022-03-07 18:40:41 -0300

    requisito 2: cria funções para lidar com o input, o botão e o redirecionamento de página

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Link, Redirect } from 'react-router-dom';
 import Album from './pages/Album';
 import Favorites from './pages/Favorites';
 import Login from './pages/Login';
@@ -7,8 +7,71 @@ import NotFound from './pages/NotFound';
 import Profile from './pages/Profile';
 import ProfileEdit from './pages/ProfileEdit';
 import Search from './pages/Search';
+import Loading from './pages/Loading';
+import { createUser } from './services/userAPI';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      isButtonDisabled: true,
+      isLoggedIn: false,
+      isLoading: false,
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleRedirect = this.handleRedirect.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+    const MIN = 3;
+    if (value.length >= MIN) {
+      this.setState({
+        [name]: value,
+        isButtonDisabled: false,
+      });
+    } else {
+      this.setState({
+        isButtonDisabled: true,
+      });
+    }
+  }
+
+  async handleSubmit() {
+    const { name } = this.state;
+    this.setState({
+      isLoading: true,
+    });
+    await createUser({ name });
+    this.setState({
+      isLoggedIn: true,
+      isLoading: false,
+    });
+  }
+
+  handleRedirect() {
+    const { isButtonDisabled, isLoggedIn, isLoading } = this.state;
+    if (isLoggedIn) {
+      return (
+        <Redirect to="/search" />
+      );
+    }
+    if (isLoading) {
+      return (
+        <Loading />
+      );
+    }
+    return (
+      <Login
+        isButtonDisabled={ isButtonDisabled }
+        handleChange={ this.handleChange }
+        handleSubmit={ this.handleSubmit }
+      />
+    );
+  }
+
   render() {
     return (
       <BrowserRouter>
@@ -18,7 +81,9 @@ class App extends React.Component {
           <Route path="/favorites" component={ Favorites } />
           <Route path="/album/:id" component={ Album } />
           <Route path="/search" component={ Search } />
-          <Route exact path="/" component={ Login } />
+          <Route exact path="/">
+            { this.handleRedirect() }
+          </Route>
           <Route path="/*" component={ NotFound } />
         </Switch>
       </BrowserRouter>
