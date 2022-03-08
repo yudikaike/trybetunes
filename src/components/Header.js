6aa80050554b19a57e54989b6815be239ad886c8
Kaike Yudi <kaikeyudi@outlook.com>
2022-03-08 07:45:58 -0300

    requisito 4: adiciona links de navegação no componente Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from './Loading';
 
@@ -30,9 +31,18 @@ class Header extends Component {
   render() {
     const { isLoading, name } = this.state;
     return (
-      <header data-testid="header-component">
-        {isLoading ? <Loading /> : <div data-testid="header-user-name">{name}</div>}
-      </header>
+      <div>
+        <header data-testid="header-component">
+          {isLoading ? <Loading /> : (
+            <div>
+              <Link to="/search" data-testid="link-to-search">Search</Link>
+              <Link to="/favorites" data-testid="link-to-favorites">Favorites</Link>
+              <Link to="/profile" data-testid="link-to-profile">Profile</Link>
+              <div data-testid="header-user-name">{name}</div>
+            </div>
+          )}
+        </header>
+      </div>
     );
   }
 }
