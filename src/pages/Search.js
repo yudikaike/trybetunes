0c8ed63cb59fde1351f185ae2b30fe55126ab8c8
Kaike Yudi <kaikeyudi@outlook.com>
2022-03-08 08:10:53 -0300

    requisito 5: adiciona formulário de pesquisa ao componente Search

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -3,10 +3,28 @@ import Header from '../components/Header';
 
 class Search extends Component {
   render() {
+    const { handleSearch, isButtonDisabled } = this.props;
     return (
       <div>
         <Header />
-        <div data-testid="page-search">Search</div>
+        <div data-testid="page-search">
+          <form>
+            <input
+              onChange={ handleSearch }
+              type="text"
+              name="search"
+              data-testid="search-artist-input"
+              placeholder="Nome do Artista"
+            />
+            <button
+              disabled={ isButtonDisabled }
+              type="button"
+              data-testid="search-artist-button"
+            >
+              Procurar
+            </button>
+          </form>
+        </div>
       </div>
     );
   }
